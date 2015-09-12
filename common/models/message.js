b935e45f115415000186ef2048922784221d1c17
jadoux <jadoux@lynkoa.com>
2015-09-12 19:20:55 +0200

    Ajout de la date de création dans les messages, c'est quand me^me bien plus praituqe Oô

--- common/models/message.js
+++ common/models/message.js
@@ -1,6 +1,7 @@
 module.exports = function(Message) {
   Message.beforeRemote('create', function(context, user, next) {
     var req = context.req;
+    req.body.created = Date.now();
     req.body.publisherId = req.accessToken.userId;
     next();
   });
