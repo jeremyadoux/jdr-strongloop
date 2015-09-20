dd0432e5ebee33f9c8020782e6f6ae48c2a959eb
jadoux <jeremy.adoux@gmail.com>
2015-09-20 21:38:25 +0200

    rajout de la gestion du redimensionnement des images à la demande, + rajout d'un username à l'utilisateur

--- server/server.js
+++ server/server.js
@@ -40,5 +40,34 @@ boot(app, __dirname, function(err) {
 
   // start the server if `$ node server.js`
   if (require.main === module)
-    app.start();
+  //Comment this app.start line and add following lines
+  //app.start();
+    app.io = require('socket.io')(app.start());
+  require('socketio-auth')(app.io, {
+    authenticate: function (value, user, callback) {
+
+      var AccessToken = app.models.AccessToken;
+      //get credentials sent by the client
+      var token = AccessToken.find({
+        where:{
+          and: [{ userId: value.userId }, { id: value.id }]
+        }
+      }, function(err, tokenDetail){
+        if (err) throw err;
+        if(tokenDetail.length){
+          callback(null, true);
+        }else{
+          callback(null, false);
+        }
+      }); //find function..
+    } //authenticate function..
+  });
+
+  app.io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+  });
 });
+
